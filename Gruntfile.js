cddbbe04e49f523cf9b6e1904bbd7ea85071043c
Adam Wentz <adam@adamwentz.com>
2015-09-24 23:38:31 -0500

    better

--- Gruntfile.js
+++ Gruntfile.js
@@ -305,7 +305,9 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'models/**/*',
+            'videos/*'
           ]
         }]
       },
